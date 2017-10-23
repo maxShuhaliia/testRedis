49b9a6accfae7ea4c34d14656c54c9cd7354335e
root <root@s2600.hqhost.net>
2017-10-23 15:10:46 +0100

    add remote address for redis

--- Connections.js
+++ Connections.js
@@ -1,14 +1,17 @@
 const redis = require('redis');
 const Config = require('./Config.json');
-const redisClient = redis.createClient();
+const redisClient = redis.createClient({host: '88.214.193.71',
+                                         port: 6379});
 
 redisClient.on("error", function (err) {
     console.log("Error " + err);
 });
 module.exports.redisClient = redisClient;
 
-let subscriber =  redis.createClient();
-let publisher =  redis.createClient();
+let subscriber =  redis.createClient({host: '88.214.193.71',
+    port: 6379});
+let publisher =  redis.createClient({host: '88.214.193.71',
+    port: 6379});
 
 
 module.exports.subscriber = subscriber
